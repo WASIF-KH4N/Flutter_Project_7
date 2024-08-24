5b77f18c369fa7daf770cf2fe43bb01fbb4e4cde
Wasif Khan <m.wasif9905@gmail.com>
2024-08-24 23:51:24 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -12,11 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false;
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home:MyHomePage(),
